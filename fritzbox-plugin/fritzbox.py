bc76daacf58fb20f7efc3e95c11e5eaa999937e1
Josip Prpić <josippr@gmail.com>
2025-07-16 20:42:28 +0200

    temporary comment out prints (#6)

--- fritzbox-plugin/fritzbox.py
+++ fritzbox-plugin/fritzbox.py
@@ -12,17 +12,17 @@ def list_connected_devices(fritz_ip=FRITZ_IP, username=FRITZ_USERNAME, password=
     try:
         fc = FritzConnection(address=fritz_ip, user=username, password=password)
         devices = fc.call_action('Hosts', 'GetHostNumberOfEntries')['NewHostNumberOfEntries']
-        print(f"Total devices found: {devices}")
+        print(f"[FRITZ] Total devices found: {devices}")
         for index in range(devices):
             device_info = fc.call_action('Hosts', 'GetGenericHostEntry', NewIndex=index)
             active = device_info.get('NewActive', False)
             if active:
-                print(f"\nDevice #{index+1}")
-                print(f"  Name      : {device_info.get('NewHostName')}")
-                print(f"  IP Address: {device_info.get('NewIPAddress')}")
-                print(f"  MAC       : {device_info.get('NewMACAddress')}")
-                print(f"  Active    : {active}")
+                # print(f"\nDevice #{index+1}")
+                # print(f"  Name      : {device_info.get('NewHostName')}")
+                # print(f"  IP Address: {device_info.get('NewIPAddress')}")
+                # print(f"  MAC       : {device_info.get('NewMACAddress')}")
+                # print(f"  Active    : {active}")
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"[FRITZ] Error: {e}")
 
 list_connected_devices(FRITZ_IP, FRITZ_USERNAME, FRITZ_PASSWORD)
